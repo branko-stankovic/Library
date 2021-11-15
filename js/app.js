23ccb497b865488b00b388abb94e6891066e332a
branko-stankovic <branko.stankovic@gmail.com>
2021-11-15 14:18:11 +0100

    Initialize myLibrary array to hold books, add a function that adds new books to the array

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
@@ -6,4 +8,9 @@ function Book(title, author, pages, read) {
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
