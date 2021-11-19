b34d6eeb54fc63c1ffe08fa3b44b7c58dad1184c
branko-stankovic <branko.stankovic@gmail.com>
2021-11-19 14:33:46 +0100

    Remove unnecessary method

--- js/app.js
+++ js/app.js
@@ -38,10 +38,7 @@ function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    }
+    this.read = read
 }
 
 function addBookToLibrary(title, author, pages, read) {
